ebe0c5c8ee0f478540831d5f72392a9c35111a5d
Suriyaa <suriyaaap13@gmail.com>
2021-08-13 22:11:57 +0530

    Made the website responsive and added js functionality

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Hello!!")
+$('#submit-btn').click(function(e){
+
+    let roll = $('#roll').val();
+    let name = $('#name').val();
+    let marks = $('#marks').val();
+
+    e.preventDefault();
+
+    if(roll===""||name===""||marks===""){
+        alert("Hey!! your have left a field unfilled");
+    }else{
+
+        let items = '<ul class="details-display"> <li>'+ roll +'</li> <li>'+ name +'</li> <li>'+ marks +'</li> </ul>';
+
+        $('#details-container').append(items);
+
+        console.log(roll, name, marks);
+
+    }
+
+});
